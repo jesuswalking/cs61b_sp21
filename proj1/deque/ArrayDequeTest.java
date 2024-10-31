8816b92579eb5028b107a654525c8e74bb8ebac6
jesuswalking <ztr0307@gmail.com>
2024-10-31 15:44:43 +0800

    Create ArrayDequeTest.java

---
+++ proj1/deque/ArrayDequeTest.java
@@ -0,0 +1,15 @@
+package deque;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/** Performs some basic AList tests. */
+public class ArrayDequeTest {
+	
+	@Test
+	public void addFirstTest() {
+		ArrayDeque<Integer> ad = new ArrayDeque<>();
+
+		
+	}
+}
